e7b0b8a9c27995539a7be87c4fd45a225f697090
Caio de Alcântara <alcantaracaio99@gmail.com>
2025-08-09 14:45:24 -0300

    Update ner.py

--- src/ner.py
+++ src/ner.py
@@ -1,11 +1,5 @@
 import spacy
-
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    from spacy.cli import download
-    download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load("en_core_web_sm")
 
 def get_entities(text):
     if len(text.strip()) == 0:
